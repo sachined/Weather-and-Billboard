2b0578446ec93b58e729c42a9dec2dc1544b0db3
Sachin Nediyanchath <sachin.nediyanchath@gmail.com>
2020-05-21 02:44:00 -0700

    Showing md files' metadata and fs inclusion
    posts.js is made to show metadata from markdown files saved in filesystem
    
    fs had to be npm install due to a string of errors when running the server

--- weather-bill/pages/index.js
+++ weather-bill/pages/index.js
@@ -2,8 +2,9 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
+import { getSortedPostsData } from '../lib/posts'
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -22,8 +23,31 @@ export default function Home() {
             <a>Go to the First Post</a>
           </Link>
         </h2>
-
+      </section>
+      <section className={utilStyles.headingMd}>…</section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
+}
+
+export async function getStaticProps()  {
+  const allPostsData = getSortedPostsData()
+  return  {
+    props: {
+      allPostsData
+    }
+  }
 }
