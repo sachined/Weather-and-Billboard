34b031c3f9a59df678837a68e6d79a74b20d55ae
Sachin Nediyanchath <sachin.nediyanchath@gmail.com>
2020-06-26 00:36:44 -0700

    world_map div was been readded to className, not class
    Special property regarding nextJS

--- weather-bill/pages/weather-seek.js
+++ weather-bill/pages/weather-seek.js
@@ -12,24 +12,26 @@ export default function Weather_Search () {
       <Head>
         <title>Weather Search</title>
       </Head>
-        <h1>Weather Seeker</h1>
-          <div className="container">
-            <div className="app-title"><p>Weather</p></div>
-            <div className="notification"></div>
-            <div className="weather-container">
-                <div className="weather-icon"><img src="/icons/unknown.png"/></div>
-                <div className="temperature-value"><p>- °<span>C</span></p></div>
-                <div className="temperature-description"><p> - </p></div>
-                <div className="location"><p>-</p></div>
+        <div className="world_map">
+          <h1>Weather Seeker</h1>
+            <div className="container">
+              <div className="app-title"><p>Weather</p></div>
+              <div className="notification"></div>
+              <div className="weather-container">
+                  <div className="weather-icon"><img src="/icons/unknown.png"/></div>
+                  <div className="temperature-value"><p>- °<span>C</span></p></div>
+                  <div className="temperature-description"><p> - </p></div>
+                  <div className="location"><p>-</p></div>
+              </div>
             </div>
-          </div>
-          <script src="/app.js"></script>
-        <h2>
-          <Link href="/music-search">
-            <a>Go to the Music Search</a>
-          </Link>
-        </h2>
-        <img src="/weather_board.jpg" alt="weather" />
+            <script src="/app.js"></script>
+          <h2>
+            <Link href="/music-search">
+              <a>Go to the Music Search</a>
+            </Link>
+          </h2>
+          <img src="/weather_board.jpg" alt="weather" />
+        </div>
     </Layout>
   )
 }
