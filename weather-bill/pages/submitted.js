2b1d8c51c5e72c907bd4992d85577794bd8f7fd3
Sachin Nediyanchath <sachin.nediyanchath@gmail.com>
2020-08-08 17:30:05 -0700

    Update submitted.js

--- weather-bill/pages/submitted.js
+++ weather-bill/pages/submitted.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Link from 'next/link'
 import Layout from '../components/layout'
 
-export default function Submission() {
+export default function Submitted() {
   return (
     <Layout>
       <Head>
