b435977b7ae474cf89e5ef0acd9e59c27b2564d9
Sachin Nediyanchath <sachin.nediyanchath@gmail.com>
2020-08-13 03:53:42 -0700

    Icons, text, and style added
    FontAwesome library was added through npm
    
    Few import lines are added to enable inclusion of icons on home page
    
    Some text are added to include more points of contact for any interested parties
    
    The input box and the button is finally connected, and the click will lead to a submission page
    
    Information will not be saved currently, needs a database for storage.

--- weather-bill/pages/index.js
+++ weather-bill/pages/index.js
@@ -4,7 +4,22 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 import { getSortedPostsData } from '../lib/posts'
 import Date from '../components/date'
-import "https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous"
+
+// import the library
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+// import your icons
+import { faGlobe } from '@fortawesome/free-solid-svg-icons'
+import {
+  faGithub,
+  faTwitter,
+  faLinkedin
+} from '@fortawesome/free-brands-svg-icons'
+library.add(
+  faGithub,
+  faTwitter,
+  faLinkedin
+)
 
 export default function Home({ allPostsData }) {
   return (
@@ -13,8 +28,8 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hi, I am Sachin. I am a web developer and a trilingual (English/Malayalam/Spanish).
-        You can contact me on <a href="https://twitter.com/nediyanchath" target="_blank">Twitter</a>.</p>
+        <p>Hi, I am Sachin. I am a web developer and a trilingual (English/Malayalam/Spanish) living in California.<br/><br/>
+        You can check out my <a href="https://www.linkedin.com/in/nediyanchath/" target="_blank"> career <FontAwesomeIcon icon={faLinkedin} style={{width: '25px'}} /></a>, where you can contact me, or direct message <a href="https://twitter.com/nediyanchath" target="_blank"> here <FontAwesomeIcon icon={faTwitter} style={{width: '25px'}} /></a>.</p>
         <h1>
           <a href="https://warm-springs-80612.herokuapp.com/">Shopify-spoofed site</a>
         </h1>
@@ -23,8 +38,17 @@ export default function Home({ allPostsData }) {
             <a>Want to know the weather?</a>
           </Link>
         </h2>
-          <input type="text" placeholder="email" required>
-          <button class="btn"><i class="fa fa-paper-plane">Submit</i></button>
+          <form action="/submitted">
+            <h3 className={utilStyles.contact}>Interested in having a conversation?</h3>
+            <div className="row">
+              <div className="col-xs-7">
+                <input type="text" placeholder="email" required />
+              </div>
+              <div className="col-xs-5">
+                <button className="btn"><FontAwesomeIcon icon={faGlobe} />Submit</button>
+              </div>
+            </div>
+          </form>
       </section>
       <section className={utilStyles.headingMd}>…</section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
@@ -43,6 +67,7 @@ export default function Home({ allPostsData }) {
           ))}
         </ul>
       </section>
+      <h4>All the apps were coded from these repositories from this <a href="https://github.com/sachined" target="_blank"> Github </a> <FontAwesomeIcon icon={faGithub} style={{width: '30px'}} />, using Git as a medium for Heroku and Vercel. </h4>
     </Layout>
   )
 }
