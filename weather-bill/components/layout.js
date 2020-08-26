668c0d7017ab0ab126dda812bff7cdb844db8cd7
Sachin Nediyanchath <sachin.nediyanchath@gmail.com>
2020-08-25 17:06:45 -0700

    Capitalize O for next.js

--- weather-bill/components/layout.js
+++ weather-bill/components/layout.js
@@ -13,7 +13,7 @@ export default function Layout({ children, home }) {
         <link rel="stylesheet"
           href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
           integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
         />
         <link rel="icon" href="/frog.ico" />
         <meta
